28ff2752fc084b99101beb6d32937ab534830336
Alex Hurley <the-Hull@users.noreply.github.com>
2020-09-03 23:00:01 +0200

    added test for posixct Range in calc_limit_per_group

--- tests/testthat/test_calc_limits_per_groups.R
+++ tests/testthat/test_calc_limits_per_groups.R
@@ -59,4 +59,49 @@ test_that("calculated limits are equal to manual approach (ungrouped iris)", {
 })
 
 
+test_that("calculated limits are equal to manual approach on time series df)", {
 
+
+    tsdf <- data.frame(x = seq(as.POSIXct("2000-01-01", tz = "UTC"),
+                       as.POSIXct("2000-02-01", tz = "UTC"),
+                       length.out = 25),
+               y = rnorm(25)) %>%
+        dplyr::mutate(group_index = dplyr::cur_group_id())
+
+
+    x <- "x"
+    y <- "y"
+
+
+
+
+    xrange <- range(tsdf[ , x])
+    yrange <- range(tsdf[ , y])
+
+
+    total_range <-  as.numeric(range(tsdf[ , x, drop = TRUE], na.rm = TRUE))
+    # offset <- (diff(total_range)) * c(-1,1) + scaling
+    offset <- (diff(total_range) * 0.01) * c(-1,1)
+
+
+    lim <- range(tsdf[ , x, drop = TRUE], na.rm = TRUE) +
+        offset
+
+    compare_list <- list(xlim = lim,
+                         ylim = yrange)
+
+
+
+
+
+
+    lim_func <- calc_limits_per_groups(dframe = tsdf ,
+                                       group_index = 1,
+                                       xvar = x,
+                                       yvar = y,
+                                       scaling = 0)
+    expect_identical(lim_func, compare_list)
+
+
+
+})
